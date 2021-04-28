a7e85db2d37f9704d4d5b32eaef76195b243c3ec
Vinicius Prado <vinicavagna@gmail.com>
2021-04-28 14:17:44 -0300

    :wrench: add jest config file

---
+++ jest.config.ts
@@ -0,0 +1,7 @@
+export default {
+  preset: 'ts-jest',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+};
