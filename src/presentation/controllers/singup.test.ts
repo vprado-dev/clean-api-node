335c25607e81695cf2b0fff6818861c4241c59a4
Vinicius Prado <vinicavagna@gmail.com>
2021-05-03 14:19:02 -0300

    :wrench: add email test

--- src/presentation/controllers/singup.test.ts
+++ src/presentation/controllers/singup.test.ts
@@ -13,4 +13,16 @@ describe('SingUp Controller', () => {
     expect(httpResponse?.statusCode).toBe(400);
     expect(httpResponse?.body).toEqual(new Error('Missing param: name'));
   });
+  test('Should return 400 if no email is provider', () => {
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = singUp(httpRequest);
+    expect(httpResponse?.statusCode).toBe(400);
+    expect(httpResponse?.body).toEqual(new Error('Missing param: email'));
+  });
 });
