f34fd4a5990f667819c67d2f89f2c12cb6c751d1
Vinicius Prado <vinicavagna@gmail.com>
2021-05-03 12:01:24 -0300

    :wrench: create singup.ts

---
+++ src/presentation/controllers/singup.test.ts
@@ -0,0 +1,16 @@
+import { SingUpController } from './singup';
+
+describe('SingUp Controller', () => {
+  test('Should return 400 if no name is provider', () => {
+    const sut = new SingUpController();
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
---
+++ src/presentation/controllers/singup.ts
@@ -0,0 +1,5 @@
+export class SingUpController {
+  handle(httpRequest: any): any {
+    return { statusCode: 400 };
+  }
+}
